12cb67f2b4a0c515879b849b74890085050d7d65
Joseph Senders <jasenders@gmail.com>
2018-06-07 18:52:28 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,4 @@
 def oxford_comma(array)
 values = ["kiwi", "durian", "starfruit", "mangos", "dragon fruits"]
 
-result = values.join[0]
-puts result
-result_2 = values.join[0-1](" and ")
-puts result_2
 end
