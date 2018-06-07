719639f3b0d825fab34a1e33e14b113de4e7e745
Joseph Senders <jasenders@gmail.com>
2018-06-07 18:58:36 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   if array.length == 1
       return "#{array[0]}"
-    elsif array.length == 2 
+    elsif array.length == 2
       return array.join (" and ")
     elsif array.length >= 3
       return new_array_item = "and #{array[-1]}"
@@ -10,4 +10,5 @@ def oxford_comma(array)
           return array.join(", ")
         end
       end
+    end
 end
